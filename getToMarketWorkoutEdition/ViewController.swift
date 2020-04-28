a09c364e39c2f5497457736fe0bc32cc2895f4a9
kiyoshi <kiyoshiwoolheater@gmail.com>
2020-04-28 16:31:08 -0500

    problem

--- getToMarketWorkoutEdition/ViewController.swift
+++ getToMarketWorkoutEdition/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelOne: UILabel!
+    @IBOutlet weak var height: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        labelOne.numberOfLines = 0
+        height.constant = 1500
     }
+    
+    
 
 
 }
